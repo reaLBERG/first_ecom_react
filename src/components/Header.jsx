849d11775c381a5f5da9bbdc9f6370b5bb6934a3
reaLBERG <73079561+reaLBERG@users.noreply.github.com>
2023-06-21 12:11:49 +0300

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light py-3">
                 <div className="container py-2">
-                <NavLink className="navbar-brand mx-auto fw-bold fs-4" to="/">Stuff Shop</NavLink>    
+                <NavLink className="navbar-brand mx-auto fw-bold fs-4" to="/first_ecom_react/">Stuff Shop</NavLink>    
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
