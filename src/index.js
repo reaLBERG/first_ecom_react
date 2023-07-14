3643e4a3aa260af9571f062655e47416a26ce947
reaLBERG <73079561+reaLBERG@users.noreply.github.com>
2023-07-14 12:33:56 +0300

    21

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import store from './redux/store'
 import {Provider} from 'react-redux'
 
 ReactDOM.render(
-  <BrowserRouter basename="/first_ecom_react">
+  <BrowserRouter>
   <Provider store={store}>
     <App />
     </Provider>
