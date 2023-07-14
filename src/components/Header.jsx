986f9273baf10336401d32d7e04647b4e20ad439
reaLBERG <73079561+reaLBERG@users.noreply.github.com>
2023-07-14 12:30:02 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,23 +9,23 @@ const Header = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light py-3">
                 <div className="container py-2">
-                <NavLink className="navbar-brand mx-auto fw-bold fs-4" to="/first_ecom_react/">Stuff Shop</NavLink>    
+                <NavLink className="navbar-brand mx-auto fw-bold fs-4" to="/">Stuff Shop</NavLink>    
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink className="nav-link" aria-current="page" to="/first_ecom_react/">Home</NavLink>
+                                <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="/first_ecom_react/products">Product</NavLink> 
+                                <NavLink className="nav-link" to="/products">Product</NavLink> 
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="/first_ecom_react/about">About</NavLink>
+                                <NavLink className="nav-link" to="/about">About</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" to="/first_ecom_react/contact">Contact</NavLink>
+                                <NavLink className="nav-link" to="/contact">Contact</NavLink>
                             </li>
                             
                             
